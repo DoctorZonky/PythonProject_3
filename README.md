61463fee2704d5042827239adedf95e22d625ed4
DoctorZonky <adrian.vitz@gmail.com>
2025-06-07 12:35:23 +0200

    Änderung der Kommentare, Kommentare neben den Zeilen zusammengefasst und über Code-Blöcke geschrieben für bessere Übersicht

--- README.md
+++ README.md
@@ -1,10 +1,32 @@
 # PythonProject_3
 
-# Wichtig vor Start der Datei
-Python muss installiert werden und dabei dem PATH zugewiesen werden
-Verwendete Bibliotheken:
-- Streamlit
-- Matplotlib
+# Vorbereitung für die Ausführung der App
+## Erste Schritte
+1) Laden Sie sich die Dateien "XXX" und "YYY" herunter und speichern sie in einem Ordner auf dem Desktop
+2) Doppelklicken Sie den Ordner, in dem Sie die Dateien gespeichert haben und öffnen ihn somit
+3) Erweitern Sie den Dateipfad mit einem Einfachklick der linken Maustaste in das Adressfeld (Beginnt mit "Desktop")
+4) Kopieren Sie den Pfad aus der Adresszeile (STRG + C)
+
+## Öffnen der Konsole 
+1) Öffnen Sie das Dialogfeld "Ausführen" mit der Tastenkombination "WIN + R"
+2) Nun sollte sich ein Suchfenster mit der Aufschrift "Ausführen" öffnen
+3) Tippen Sie hier "cmd" ein und drücken Sie Enter
+4) Ein schwarzes Fenster (die Konsole) öffnet sich
+
+## Installation der Dependencies
+1) Tippen Sie in die Konsole "cd " und fügen das zuvor kopierte Directory aus dem Zwischenspeicher ein (STRG + V)
+2) Drücken Sie Enter, nun sollte der ausgewählte Ordner in der Konsole als angesteuerter Pfad angezeigt werden
+3) Tippen Sie nun 'pip install -r "requirements.txt"' ("" müssen hier mit kopiert werden!)
+4) Alle notwendigen Libraries sollten nun installiert werden
+
+## Ausführen des Programms
+1) Tippen Sie nun in die Konsole 'streamlit run "YYY.py"' ("" müssen hier mit kopiert werden!)
+2) Nun sollte sich Ihr favorisierter Browser öffnen und die App in einem Browser-Tab starten.
+
+
+# Bedienung der App
+## Einlesen einer FASTA-Sequenz
+
 
 ### Einlesen einer FASTA Sequenz
 Prompt 1 (01.06.2025, 12:55 Uhr)
--- Test 2.py
+++ Test 2.py
@@ -1,82 +1,81 @@
 # Wichtige Libraries, die importiert werden müssen #
-!pip install 
-
 import numpy as np
 import streamlit as st
 import pandas as pd
 import altair as alt
-from io import StringIO
 
 
 
 # Hauptklasse FASTA definieren
+## Konstruktor der Klasse FASTA, der Attribute festlegt
 class FASTA:
-    def __init__(self, fasta_input=""):
+    def __init__(self, fasta_input=""):                                                                     
         self.fasta_input = fasta_input
         self.fasta_seq = ""
         self.fasta_header = ""
         self.fasta_inverted = ""
         self.fasta_base_counts = {}
 
+    ## Methode, um die hochgeladene Datei in einen String umzwandeln und intern zu speichern
     def fasta_file_to_string(self):
-        self.fasta_input = self.fasta_input.getvalue().decode("utf-8")                              # Die hochgeladene Datei wird in einen String umgewandelt und intern gespeichert
+        self.fasta_input = self.fasta_input.getvalue().decode("utf-8")                                      
 
-    ## Methode zum Aufreinigen eines FASTA-Textes zum Erzeugen eines Sequenz-Strings
+    ## Methode zum Aufreinigen eines FASTA-Textes und der Erzeugung eines Sequenz-Strings durch Entfernung des Headers
     def get_sequence(self):
-        lines = self.fasta_input.splitlines()                                                       # Der eingegebene Text wird in separate Lines gesplittet
-        seq_lines = [line.strip() for line in lines if not line.startswith(">")]                    # Entfernung des Headers 
-        self.fasta_seq = "".join(seq_lines).replace(" ", "").upper()                                # Die Linien werden in eine Sequenz gemerget und (falls nötig) in Großbuchstaben umgewandelt, Leerzeichen zwischen den Zeilen werden entfernt
+        lines = self.fasta_input.splitlines()                                                               
+        seq_lines = [line.strip() for line in lines if not line.startswith(">")]                            
+        self.fasta_seq = "".join(seq_lines).replace(" ", "").upper()                                        
     
-    ## Methode zur Extraktion der Header Zeilen
+    ## Methode zur Extraktion der Header Zeilen, alle weiteren Zeilen werden ignoriert
     def get_header(self):
-        lines = self.fasta_input.splitlines()                                                       # Der eingegebene Text wird in separate Lines gesplittet
-        self.fasta_header = [line.strip() for line in lines if line.startswith(">")]                # Entfernung aller Zeilen außer dem Header
+        lines = self.fasta_input.splitlines()                                                               
+        self.fasta_header = [line.strip() for line in lines if line.startswith(">")]
            
-    ## Methode zur Erzeugung eines inversen DNA-Strangs in 5'-3'-Richtung
+    ## Methode zur Erzeugung eines inversen DNA-Strangs in 5'-3'-Richtung mittels Komplement-Dictionary
     def invert_sequence(self):
-        sequence = self.fasta_seq                                                                   # Abgreifen des Attributs für den Sequenz-String
-        complement = {"A": "T", "T": "A", "C": "G", "G": "C"}                                       # Definition der komplementären Basen
-        self.fasta_inverted = "".join(complement[base] for base in reversed(sequence))              # Umkehrung der Sequenz und Ersetzung der Basen durch ihre Komplementäre
+        sequence = self.fasta_seq
+        complement = {"A": "T", "T": "A", "C": "G", "G": "C"}
+        self.fasta_inverted = "".join(complement[base] for base in reversed(sequence))
             
-    ## Methode zum Zählen der Basen in der eingegebenen Sequenz
+    ## Methode zum Zählen der Basen in der eingegebenen Sequenz durch eine for-Schleife in einem Dictionary
     def base_count(self):
-        sequence = self.fasta_seq                                                                   # Abgreifen des Attributs für den Sequenz-String
-        self.fasta_base_counts = {base: sequence.count(base) for base in "ATCG"}                    # Zählen der Basen in der Sequenz
+        sequence = self.fasta_seq
+        self.fasta_base_counts = {base: sequence.count(base) for base in "ATCG"}
         
 
 
 # Funktionen, die aufgerufen werden sollen #
 
-## Funktion zum Darstellen des Headers aus der eingegebenen Sequenz ##
+## Funktion zum Darstellen des Headers aus der eingegebenen Sequenz in einem Container (borders) ##
 def do_header(header):
-    st.header("FASTA-Sequence header")                                                              # Header der Fasta-Sequenz
-    st.container(border=True).write(f"{header}")                                                    # Ausgabe des Headers, der im Objekt FASTA-file gespeichert ist in einem Container
+    st.header("FASTA-Sequence header")
+    st.container(border=True).write(f"{header}")
 
 
-## Funktion zum Erstellen der linken Spalte (Col1) für die Anzeige der nativen Sequenz ##
+## Funktion zum Erstellen der linken Spalte (Col1) für die Anzeige der Überschrift und der nativen Sequenz in einem Expander ##
 def do_col1(sequence):
-    col1.header("Original")                                                                         # Header der Spalte
-    col1.expander("Expand / Collapse DNA Sequence", True).write(sequence)                           # Anzeige der nativen Sequenz in der ersten (linken) Spalte
+    col1.header("Original")
+    col1.expander("Expand / Collapse DNA Sequence", True).write(sequence)
 
-## Funktion zum Erstellen der rechten Spalte (Col2) für die Anzeige der inversen Sequenz ##
+## Funktion zum Erstellen der rechten Spalte (Col2) für die Anzeige der Überschrift und der inversen Sequenz in einem Expander ##
 def do_col2(sequence_inv):
     col2.header("Inverted")  
-    col2.expander("Expand / Collapse DNA Sequence", True).write(sequence_inv)                       # Ausgabe des inversen DNA-Strangs in der zweiten (rechten) Spalte als kollabierbarer Bereich
+    col2.expander("Expand / Collapse DNA Sequence", True).write(sequence_inv)
 
 
 
-## Funktion zum Wählen der Farben für das Balkendiagramm ##
+## Funktion zur Erzeugung einer Liste der Farbauswahlen für das Balkendiagramm über Hex-Farbcode-Picker ##
 def do_color_palette():
     with st.sidebar.container(border=True):
-        color_A = st.color_picker("Select color for A", "#2DE6FF")                                # Farbauswahl für A-Balken des Balkendiagramms
-        color_C = st.color_picker("Select color for C", "#2296B3")                                # Farbauswahl für C-Balken des Balkendiagramms
-        color_G = st.color_picker("Select color for G", "#3284FF")                                # Farbauswahl für G-Balken des Balkendiagramms
-        color_T = st.color_picker("Select color for T", "#1E5CB9")                                # Farbauswahl für T-Balken des Balkendiagramms
-    col_pal = [color_A, color_C, color_G, color_T]                                                  # Zusammenführen der ausgewählten Farben in eine Liste (Palette)
-    return col_pal                                                                                  # Ausgabe der Palette mit den gewählten Farben
+        color_A = st.color_picker("Select color for A", "#2DE6FF")
+        color_C = st.color_picker("Select color for C", "#2296B3")
+        color_G = st.color_picker("Select color for G", "#3284FF")
+        color_T = st.color_picker("Select color for T", "#1E5CB9")
+    col_pal = [color_A, color_C, color_G, color_T]
+    return col_pal
 
-## Funktion zur Erstellung des Barplots ##
-def do_bar_plot(base_counts_df, col_pal):
+## Funktion zur Erstellung des Barplots über ein Altair-Chart nach Documentation examples ##
+def do_bar_plot(base_counts_df, col_pal):                                                                   
       
     st.altair_chart(
         alt.Chart(base_counts_df.reset_index())
@@ -89,86 +88,87 @@ def do_bar_plot(base_counts_df, col_pal):
         )
     )
 
-## Funktion für die Ausführung zur Erstellung und Anzeige des Balkendiagramms ##
+## Funktion für die Erzeugung des Balkendiagramms über die User-Farbpalette und ein "pandas" Base-Cound-Dataframe ##
 def show_base_count_plot(base_counts):
     st.header("Base count bar plot (original sequence)")           
-    col_pal = do_color_palette()                                                                    # Aufruf der Funktion zur Erzeugung der Farbpalette für das Balkendiagramm
-    base_counts_df = pd.DataFrame.from_dict(base_counts, orient='index', columns=['Count'])         # Umwandlung des Dictionaries in ein DataFrame für die Darstellung im Balkendiagramm
+    col_pal = do_color_palette()
+    base_counts_df = pd.DataFrame.from_dict(base_counts, orient='index', columns=['Count'])
 
     st.write(base_counts_df)
-    do_bar_plot(base_counts_df, col_pal)                                                            # Aufruf der Funktion zum Erstellen des Balkendiagramms mit gewählten Farben
+    do_bar_plot(base_counts_df, col_pal)
                                                                                                     
 
-# Aufbau der App-Page #
-st.title("Analyze FASTA-DNA-Sequences")                                                             # Titel der Seite
-st.sidebar.title("Options")                                                                         # Sidebar für die Auswahl der gewünschten Funktionen
+# Aufbau der App-Page mit Titel der App und Generierung der Sidebar #
+st.title("Analyze FASTA-DNA-Sequences")
+st.sidebar.title("Options")
 
 
 
-# Prüfung auf Upload und konsekutivem Erstellen eines Objekts der Klasse FASTA #
+# Prüfung auf gewollten Dateiupload und Ausführen der Uploader-Methode für einzelne Dateien mit Typbeschreibung #
 if st.sidebar.container(border = True).toggle("Load FASTA Sequence from file", False):
-    file_uploaded = st.sidebar.file_uploader(                                                               # Sidebar-Datei-Upload für FASTA-Dateien
+    file_uploaded = st.sidebar.file_uploader(
         "Drag and drop your file here:",
         type=["fasta", "fa", "mpfa", "fna", "fsa"]
         )
 
-    ## Kontrollstruktur für die Anzeige der Sequenzen und des Headers in der App ##
-    if file_uploaded is not None:                                                                           # Prüfung, ob eine Datei hochgeladen wurde
-        FASTA_file = FASTA(file_uploaded)                                                                   # Erstellen eines FASTA-Objekts mit der hochgeladenen Datei
-        FASTA_file.fasta_file_to_string()                                                                   # Das hochgeladene FASTA-File wird in einen String umgewandelt und intern gespeichert
-        FASTA_file.get_sequence()                                                                           # Aufruf der Methode zum Extrahieren der Sequenz aus dem FASTA-String
+    ## Kontrollstruktur falls Datei hochgeladen wurde mit folgenden Methodenaufrufen für Stringumwandlung und Sequenzextraktion ##
+    if file_uploaded is not None:
+        FASTA_file = FASTA(file_uploaded)
+        FASTA_file.fasta_file_to_string()
+        FASTA_file.get_sequence()
         
         ### Layout der ersten Spalte und der Anzeige des Sequenz-Headers darüber ###
-        FASTA_file.get_header()                                                                             # Aufruf der Methode zum Herausziehen des Headers
-        do_header(FASTA_file.fasta_header)                                                                                         # Aufruf der Funktion zum Anzeigen des Headers der Sequenz
+        FASTA_file.get_header()
+        do_header(FASTA_file.fasta_header)
+        
+        ### Erzeugung der zwei Spalten für die Anzeige auf der Hauptseite und Ausführung der Generierungsfunktionen für die linke Spalte ###
+        col1, col2 = st.columns(2)
+        do_col1(FASTA_file.fasta_seq)
 
-        col1, col2 = st.columns(2)                                                                          # Erzeugen der zwei Spalten für die Anzeige auf der Hauptseite
-        do_col1(FASTA_file.fasta_seq)                                                                                           # Aufruf der Funktion zum Erstellen der linken Spalte
+        ### Toogler für die Auswahl der Anzeige des inversen DNA-Strangs und der Basenzählung ###
+        invert_DNA_on = st.sidebar.toggle("Show inverted DNA-Sequence", False)
+        base_count_on = st.sidebar.toggle("Show base count and bar plot", False)
 
-        invert_DNA_on = st.sidebar.toggle("Show inverted DNA-Sequence", False)                              # Toggle-Button für die Anzeige des inversen DNA-Strangs
-        base_count_on = st.sidebar.toggle("Show base count and bar plot", False)                            # Toggle-Button für die Anzeige des inversen DNA-Strangs
-
-        ### Kontrollstruktur zur Ausführung der Invertierung der DNA-Sequenz, falls gewünscht ###
+        ### Kontrollstruktur zur Ausführung der Invertierung der DNA-Sequenz, falls gewünscht und der Generierung der rechten Spalte ###
         if invert_DNA_on and FASTA_file is not None:
             FASTA_file.invert_sequence()
             do_col2(FASTA_file.fasta_inverted)
 
 
-        ### Kontrollstruktur zur Ausführung der Basenzählung und der Anzeige im Balkendiagramm ###
+        ### Kontrollstruktur zur Ausführung der Basenzählung und der Anzeige im Balkendiagramm mit eigenen Farben, falls gewünscht ###
         if base_count_on and FASTA_file is not None:
-            FASTA_file.base_count()                                                                         # Aufruf der Methode des Objekts FASTA_file zum Zählen der Basen und dem internen Speichern
+            FASTA_file.base_count()
             show_base_count_plot(FASTA_file.fasta_base_counts)
 
 
 # Wenn kein Upload gewollt, dann wird die manuelle Eingabe der Sequenz freigeschaltet #    
 else:
-    fasta_seq_input = st.sidebar.text_area("Gib deine FASTA-DNA-Sequenz manuell ein:")                      # Aufruf des Textfeldes, um eine FASTA-Sequenz manuell eingeben zu können
+    ## Aufruf der Methode zum Erstellen eines Texteingabefeldes für mehrere Zeilen ##
+    fasta_seq_input = st.sidebar.text_area("Gib deine FASTA-DNA-Sequenz manuell ein:")
 
-    ## Kontrollstruktur, wenn Sequenz manuell eingegeben wurde ##
-    if fasta_seq_input is not None and fasta_seq_input.strip() != "":                                       # Prüfung, ob eine Sequenz eingegeben wurde
-        FASTA_input = FASTA(fasta_seq_input)                                                                # Erstellen eines neuen FASTA-Objekts aus dem manuellen Input
-        FASTA_input.get_sequence()                                                                          # Aufruf der Methode zum Extrahieren der Sequenz aus dem Input
-        FASTA_input.get_header()                                                                            # Aufruf der Methode zum Herausziehen des Headers
-        do_header(FASTA_input.fasta_header)                                                                 # Aufruf der Funktion zum Anzeigen des Headers auf der Hauptseite der App
+    ## Kontrollstruktur, ob Sequenz manuell eingegeben wurde und folgender Methodenaufrufe zum Erhalt der Sequenz und des Headers ##
+    if fasta_seq_input is not None and fasta_seq_input.strip() != "":
+        FASTA_input = FASTA(fasta_seq_input)
+        FASTA_input.get_sequence()
+        FASTA_input.get_header()
+        do_header(FASTA_input.fasta_header)
 
-        col1, col2 = st.columns(2)                                                                          # Erzeugen der zwei Spalten für die Anzeige auf der Hauptseite
-        do_col1(FASTA_input.fasta_seq)                                                                      # Aufruf der Funktion zum Erstellen der linken Spalte
+        ### Erzeugung der zwei Spalten für die Anzeige auf der Hauptseite und Ausführung der Generierungsfunktionen für die linke Spalte
+        col1, col2 = st.columns(2)
+        do_col1(FASTA_input.fasta_seq)
+
+        ### Toogler für die Auswahl der Anzeige des inversen DNA-Strangs und der Basenzählung ###
+        invert_DNA_on = st.sidebar.toggle("Show inverted DNA-Sequence", False)
+        base_count_on = st.sidebar.toggle("Show base count and bar plot", False)
 
 
-        invert_DNA_on = st.sidebar.toggle("Show inverted DNA-Sequence", False)                              # Toggle-Button für die Anzeige des inversen DNA-Strangs
-        base_count_on = st.sidebar.toggle("Show base count and bar plot", False)                            # Toggle-Button für die Anzeige des inversen DNA-Strangs
-
-
-        ### Kontrollstruktur zur Ausführung der Invertierung der DNA-Sequenz, falls gewünscht ###
+        ### Kontrollstruktur zur Ausführung der Invertierung der DNA-Sequenz, falls gewünscht und Generierung der rechten Spalte ###
         if invert_DNA_on and fasta_seq_input is not None:
             FASTA_input.invert_sequence()
             do_col2(FASTA_input.fasta_inverted)
 
 
-        ### Kontrollstruktur zur Ausführung der Basenzählung und der Anzeige im Balkendiagramm ###
+        ### Kontrollstruktur zur Ausführung der Basenzählung und der Anzeige im Balkendiagramm mit eigenen Farben, falls gewünscht ###
         if base_count_on and fasta_seq_input is not None:
-            FASTA_input.base_count()                                                                         # Aufruf der Methode des Objekts FASTA_file zum Zählen der Basen und dem internen Speichern
+            FASTA_input.base_count()
             show_base_count_plot(FASTA_input.fasta_base_counts)
-
-
-############################################### Bis hier hin überarbeitet ####################################################
